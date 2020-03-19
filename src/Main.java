6a8d067b16c1def76e85a4cb1c395049dd4cf8ea
Scarcrux <reg@scarcrux.com>
2020-03-18 22:27:36 -0400

    All Specs Met

--- src/Main.java
+++ src/Main.java
@@ -5,36 +5,48 @@
  */
 
 import java.io.*;
-import java.util.*;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
+/* Binary Search Tree for Movies */
 class MovieBST {
   private MovieNode root;
+  private StringBuilder sb = new StringBuilder();
 
-  // gets root movie
+  /* Gets root movie */
   public MovieNode getRoot() {
     return root;
   }
 
-  // inserts element into tree
+  /* Inserts movie into tree */
   public void put(String key, int value) {
     if (root == null) {
-      root = new MovieNode( key, value );
+      root = new MovieNode(key, value);
     } else {
-      root.put( key, value );
+      root.put(key, value);
     }
   }
 
-  // finds key and returns value if it exists
+  /* Searches for key and attempts to return a value */
   public String get(String key) {
     return root == null ? null : root.get(key);
   }
 
-  // Selects movie titles that fall alphabetically between start and end
+  /* Clears the StringBuilder used for CSV output */
+  public void PrintClear() {
+    sb.setLength(0);
+  }
+  /* Selects movie titles that fall alphabetically between start and end */
   public void Print(MovieNode node, String title1, String title2) {
-    /* base case */
+    // Base Case: null node
     if (node == null) {
+      // Writes the playlist to CSV
+      File csvOutputFile = new File("../data/reports/" + title1 + "_" + title2 + ".csv");
+      try (PrintWriter  writer= new PrintWriter(csvOutputFile)) {
+        writer.write(sb.toString());
+      } catch (FileNotFoundException error) {
+        System.out.println(error);
+      }
       return;
     }
 
@@ -43,76 +55,81 @@ class MovieBST {
         Print(node.getLeft(), title1, title2);
     }
 
-    /* If root is within range, then print the title */
+    // If root is within range, then print the title
     if ((title1.compareTo(node.getTitle()) < 0
       || title1.compareTo(node.getTitle()) == 0)
       && (title2.compareTo(node.getTitle()) > 0
       || title2.compareTo(node.getTitle()) == 0)) {
+      sb.append(node.getTitle());
+      sb.append('\n');
       System.out.print(node.getTitle() + " ");
     }
 
-    /* If root is smaller than title2, then check right subtree */
+    // If root is smaller than title2, then check right subtree
     if (title2.compareTo(node.getTitle()) > 0) {
       Print(node.getRight(), title1, title2);
     }
   }
 }
 
+/* Movie nodes for the BST */
 class MovieNode {
   private String title;
   private int releaseYear;
   private MovieNode left, right;
 
+  /* Constructor for a movie with title and release year */
   public MovieNode(String title, int releaseYear) {
     this.title = title;
     this.releaseYear = releaseYear;
   }
 
-  //if key not found in MovieBST then it is added. if key already exists then that node's value
-  //is updated.
-  public void put( String key, int releaseYear) {
-    if ( key.compareTo( this.title ) < 0 ) {
-      if ( left != null ) {
-        left.put( key, releaseYear);
+  /* Adds a new movie node or updates an existing one */
+  public void put(String key, int releaseYear) {
+    // Checks whether the key is alphabetically less than
+    if (key.compareTo(this.title) < 0 ) {
+      if (left != null) {
+        left.put(key, releaseYear);
       } else {
-        left = new MovieNode( key, releaseYear);
+        left = new MovieNode(key, releaseYear);
       }
-    } else if ( key.compareTo( this.title ) > 0 ) {
+    } // Checks whether the key is alphabetically greater
+    else if (key.compareTo(this.title) > 0) {
       if ( right != null ) {
-        right.put( key, releaseYear );
+        right.put(key, releaseYear);
       } else {
-        right = new MovieNode( key, releaseYear );
+        right = new MovieNode(key, releaseYear);
       }
-    } else {
+    } // Updates the movie because it's the same
+    else {
       this.releaseYear = releaseYear;
     }
   }
 
-  // returns movie title
+  /* Gets movie title */
   public String getTitle() {
     return title;
   }
 
-  // returns left node
+  /* Gets left node */
   public MovieNode getLeft() {
     return left;
   }
 
-  // returns right node
+  /* Gets right node */
   public MovieNode getRight() {
     return right;
   }
 
-  // finds Node with given key and return its value
+  /* Searches for movie and attempts to return a value */
   public String get(String key) {
     if (this.title.equals(key)) {
       return title + " "+ releaseYear;
     }
-
     if (key.compareTo(this.title) < 0) {
       return left == null ? null : left.get(key);
     } else {
-      return right == null ? null : right.get( key );
+      return right == null ? null : right.get(key);
     }
   }
 }
@@ -120,35 +137,30 @@ class MovieNode {
 /* Test class */
 public class Main {
   public static void main(String[] args) {
-    // Creates an array in which we will store the names of files and directories
+    // Creates an array to store the names of files and directories
     String[] myFiles;
 
-    // Creates a new File instance by converting the given pathname string
-    // into an aMovieBSTract pathname
+    // Creates a new File instance by converting the given pathname String
+    // into an abstract pathname
     String path = "../data/";
     File f = new File(path);
 
-      // This filter will only include files ending with .py
+   // Filters to only include CSV files
     FilenameFilter filter = new FilenameFilter() {
       @Override
       public boolean accept(File f, String name) {
         return name.endsWith(".csv");
       }
     };
+    myFiles = f.list(filter);
 
-   myFiles = f.list(filter);
-
-    // appends the path to the filename
+    // Appends the path to the filename
     for (int i = 0; i < myFiles.length; i++) {
       myFiles[i] = path + myFiles[i];
     }
 
-    for (String element: myFiles) {
-      System.out.println(element);
-    }
-
+    // Creates the BST and populates it with data
     MovieBST tree = new MovieBST();
-
     String csvFile = myFiles[0];
     BufferedReader br = null;
     String line = "";
@@ -158,25 +170,23 @@ public class Main {
       br = new BufferedReader(new FileReader(csvFile));
       while ((line = br.readLine()) != null) {
         // Uses comma as a delimiter
-        String[] country = line.split(cvsSplitBy);
+        String[] movies = line.split(cvsSplitBy);
 
-        String temp = country[1];
-        String regex = "^([^()]*)\\((()]*)\\)(.*)$";
+        String temp = movies[1];
         Pattern p = Pattern.compile("\\((\\d{4}?)\\)$");
         Pattern p2 = Pattern.compile("^([^()]*)");
         Matcher m = p.matcher(temp);
         Matcher m2 = p2.matcher(temp);
 
-        // if regex matches found then put movie in tree
+        // If regex matches found, then inserts movie into tree
         while(m.find() && m2.find()) {
-          if (country[1].equals("title")) {
+          if (movies[1].equals("title")) {
 
-          } else if (country[1] == null) {
+          } else if (movies[1] == null) {
           } else {
               tree.put(m2.group(1).trim(), Integer.parseInt(m.group(1).trim()));
 
           }
-          System.out.println(m2.group(1) + "" + m.group(1));
         }
       }
     } catch (FileNotFoundException error) {
@@ -192,10 +202,12 @@ public class Main {
         }
       }
     }
-    // test output
+
+    // Test Output
     System.out.println(tree.get("Flint"));
-    tree.Print(tree.getRoot(), "Flint", "Harry Potter");
-    System.out.println("");
-    tree.Print(tree.getRoot(), "Superman", "Xeno");
+    tree.Print(tree.getRoot(), "Superman", "Zeus");
+    tree.PrintClear();
+    System.out.println("\n");
+    tree.Print(tree.getRoot(), "Harry Potter", "John");
   }
 }
